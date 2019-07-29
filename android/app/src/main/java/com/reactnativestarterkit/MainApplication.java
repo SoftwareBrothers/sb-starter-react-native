d6a8ee6bd43c1a328d2c3dcd8e3aee622f4c674d
Adam Lachmann <adam.lachman@rst-it.com>
2019-07-29 13:01:17 +0200

    chore: removed manually linked rn-fetch-blob

--- android/app/src/main/java/com/reactnativestarterkit/MainApplication.java
+++ android/app/src/main/java/com/reactnativestarterkit/MainApplication.java
@@ -7,7 +7,6 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -27,8 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-
-      packages.add(new RNFetchBlobPackage());
 
       return packages;
     }
